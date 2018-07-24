bd8e6fef835de2f823f71d514c9567c2903b11eb
tjyoung99 <40397667+tjyoung99@users.noreply.github.com>
2018-07-24 14:22:58 -0500

    sss

--- Arcade1.pyde
+++ Arcade1.pyde
@@ -8,7 +8,13 @@ def setup():
     
 def draw():
     man()
-
+    textSize(32)
+    text("apple",25,35)
+    squarea(25,18)
+    squarep(43,20)
+    squarep(63,20)
+    squarel(83,10)
+    squaree(93,20)
 
 
 
--- Arcade2.py
+++ Arcade2.py
@@ -47,3 +47,52 @@ def hangman():
     themeSetup()
     platform()
     logo()
+    
+a=False
+p=False
+l=False
+e=False
+def squarea(x,w):
+    global a, p, l, e
+    if a:
+        noFill()
+    else:
+        fill(0)
+    if keyPressed:
+        if key == 'a' :
+            a=True   
+    rect(x, 10, w, 30)
+
+def squarep(x,w):
+    global a, p, l, e
+    if p:
+        noFill()
+    else:
+        fill(0)
+    if keyPressed:
+        if key == 'p' :
+            p=True   
+    rect(x, 10, w, 30)
+    
+    
+def squarel(x,w):
+    global a, p, l, e
+    if l:
+        noFill()
+    else:
+        fill(0)
+    if keyPressed:
+        if key == 'l' :
+            l=True   
+    rect(x, 10, w, 30)
+    
+def squaree(x,w):
+    global a, p, l, e
+    if e:
+        noFill()
+    else:
+        fill(0)
+    if keyPressed:
+        if key == 'e' :
+            e=True   
+    rect(x, 10, w, 30)
